d24d80dd063f62bf9142f2b23c4c93c8d7ec69b7
nanderstabel <nanderstabel@gmail.com>
2021-12-03 19:23:46 +0100

    Add d02-2

--- src/bin/d02-1.rs
+++ src/bin/d02-1.rs
@@ -3,6 +3,7 @@ use aoc::{Direction::*, *};
 fn main() {
     let input: Vec<Command> = read("input/d02");
     let mut directions = [0u32; 3];
+
     for command in input {
         directions[command.0] += command.1;
     }
