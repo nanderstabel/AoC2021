84c3b0286ef2d67d68d2f76ec9d6b91e1c93c4d6
nanderstabel <nanderstabel@gmail.com>
2021-12-04 19:59:14 +0100

    Add d03 part 1

--- src/bin/d03.rs
+++ src/bin/d03.rs
@@ -1,31 +1,33 @@
 use aoc::*;
-use derive_more::{BitAnd, BitOrAssign, Shl, ShlAssign};
+use derive_more::{BitAnd, BitOrAssign, Display, Mul, Not, Shl};
 use std::str::FromStr;
 
-#[derive(Debug, Copy, Clone, BitAnd, Shl, ShlAssign, BitOrAssign, PartialEq, PartialOrd)]
-pub struct Binary(u16);
+#[derive(
+	Debug, Display, Copy, Clone, BitAnd, Shl, BitOrAssign, PartialEq, PartialOrd, Not, Mul,
+)]
+#[mul(forward)]
+pub struct Binary(u32);
 
 impl FromStr for Binary {
 	type Err = ();
 
 	fn from_str(s: &str) -> Result<Self, Self::Err> {
-		Ok(Binary(u16::from_str_radix(s, 2).unwrap()) << 0)
+		Ok(Binary(u32::from_str_radix(s, 2).unwrap()) << 0)
 	}
 }
 
 fn main() {
 	let input: Vec<Binary> = read("input/d03");
-	let size = input.len();
-	let mut gamma: Binary = Binary(0);
+	let mut gamma = Binary(0);
 
 	for sh in 0..12 {
-		gamma |= Binary(1);
-		gamma <<= 1;
-		input
+		if input
 			.iter()
-			.filter(|b| (**b & Binary(1 << sh)) > Binary(0))
-			.count();
-		println!("{:?}", gamma);
+			.filter(|b| (**b & Binary(1 << sh)) != Binary(0))
+			.count() > input.len() / 2
+		{
+			gamma |= Binary(1 << sh);
+		}
 	}
-	output!(1, 2);
+	output!(gamma * (!gamma & Binary(0b1111_11111111u32)), 0);
 }
