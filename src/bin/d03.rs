058aff97b43c4ba514eba9fb0fd4fae537aadc8d
nanderstabel <nanderstabel@gmail.com>
2021-12-05 18:20:15 +0100

    Add closure

--- src/bin/d03.rs
+++ src/bin/d03.rs
@@ -26,10 +26,10 @@ fn get_rating(metric: Metric, (one, zero): (Vec<&Binary>, Vec<&Binary>), sb: Bin
 		($criteria:tt) => {
 			get_rating(
 				metric,
-				match one.len() $criteria zero.len() {
-					true => one.into_iter().partition(|b| **b & sb != Binary(0)),
-					false => zero.into_iter().partition(|b| **b & sb != Binary(0)),
-				},
+				(match one.len() $criteria zero.len() {
+					true => one,
+					false => zero,
+				}).into_iter().partition(|b| **b & sb != Binary(0)),
 				sb >> 1,
 			)
 		};
@@ -46,26 +46,21 @@ fn get_rating(metric: Metric, (one, zero): (Vec<&Binary>, Vec<&Binary>), sb: Bin
 
 fn main() {
 	let input: Vec<Binary> = read("input/d03");
-	let input = &input.iter().map(|b| b).collect::<Vec<&Binary>>();
 	let mut sb = Binary(1);
 	let mut gamma = Binary(0);
 
 	while sb < Binary(1 << 11) {
-		if input.iter().filter(|b| (***b & sb) != Binary(0)).count() > input.len() / 2 {
+		if input.iter().filter(|b| (**b & sb) != Binary(0)).count() > input.len() / 2 {
 			gamma |= sb;
 		}
 		sb <<= 1;
 	}
+	let input = &input.iter().map(|b| b).collect::<Vec<&Binary>>();
+	let select = |b: &&&Binary| ***b & sb != Binary(0);
+
 	output!(
 		gamma * (!gamma & Binary(0b1111_11111111u32)),
-		get_rating(
-			Metric::O2,
-			input.into_iter().partition(|b| ***b & sb != Binary(0)),
-			sb >> 1,
-		) * get_rating(
-			Metric::CO2,
-			input.into_iter().partition(|b| ***b & sb != Binary(0)),
-			sb >> 1,
-		)
+		get_rating(Metric::O2, input.iter().partition(select), sb >> 1,)
+			* get_rating(Metric::CO2, input.iter().partition(select), sb >> 1,)
 	);
 }
