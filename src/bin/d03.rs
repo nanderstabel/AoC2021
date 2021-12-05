d9b03f73815512afee62dfb74657229213211ecc
nanderstabel <nanderstabel@gmail.com>
2021-12-05 17:09:25 +0100

    Add d03 part 2

--- src/bin/d03.rs
+++ src/bin/d03.rs
@@ -1,16 +1,58 @@
 use aoc::*;
-use derive_more::{BitAnd, BitOrAssign, Display, Mul, Not, Shl};
+use derive_more::{BitAnd, BitOrAssign, BitXorAssign, Display, Mul, Not, Shl, Shr};
 use std::str::FromStr;
 
-#[derive(Display, Copy, Clone, BitAnd, Shl, BitOrAssign, PartialEq, PartialOrd, Not, Mul)]
+#[derive(
+	Display,
+	Copy,
+	Clone,
+	BitAnd,
+	Shl,
+	Shr,
+	BitOrAssign,
+	PartialEq,
+	PartialOrd,
+	Not,
+	Mul,
+	BitXorAssign,
+)]
 #[mul(forward)]
-pub struct Binary(u32);
+struct Binary(u32);
 
 impl FromStr for Binary {
 	type Err = ();
 
 	fn from_str(s: &str) -> Result<Self, Self::Err> {
-		Ok(Binary(u32::from_str_radix(s, 2).unwrap()) << 0)
+		Ok(Binary(u32::from_str_radix(s, 2).unwrap()))
+	}
+}
+
+enum Metric {
+	O2,
+	CO2,
+}
+
+fn get_rating(metric: Metric, input: Vec<&Binary>, sh: Binary) -> Binary {
+	if input.len() == 1 {
+		return *input[0];
+	}
+	let (one, zero): (_, Vec<&Binary>) = input.into_iter().partition(|b| **b & sh != Binary(0));
+
+	macro_rules! get_rating {
+		($criteria:tt) => {
+			get_rating(
+				metric,
+				match one.len() $criteria zero.len() {
+					true => one,
+					false => zero,
+				},
+				sh >> 1,
+			)
+		};
+	}
+	match metric {
+		Metric::O2 => get_rating!(>=),
+		Metric::CO2 => get_rating!(<),
 	}
 }
 
@@ -27,5 +69,16 @@ fn main() {
 			gamma |= Binary(1 << sh);
 		}
 	}
-	output!(gamma * (!gamma & Binary(0b1111_11111111u32)), 0);
+	output!(
+		gamma * !gamma & Binary(0b1111_11111111u32),
+		get_rating(
+			Metric::O2,
+			input.iter().map(|b| b).collect(),
+			Binary(1 << 11),
+		) * get_rating(
+			Metric::CO2,
+			input.iter().map(|b| b).collect(),
+			Binary(1 << 11),
+		)
+	);
 }
