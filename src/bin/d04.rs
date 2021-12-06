62d2c55ff68a3f2a6a22f1cacf3b1f6673b49ea2
nanderstabel <nanderstabel@gmail.com>
2021-12-06 03:09:46 +0100

    Implement input split

--- src/bin/d04.rs
+++ src/bin/d04.rs
@@ -1,6 +1,28 @@
 use aoc::*;
 
+fn split_input(input: &Vec<String>) -> (Vec<u8>, Vec<Vec<i8>>) {
+	(
+		input[0]
+			.split(",")
+			.map(|s| s.parse::<u8>().unwrap())
+			.collect(),
+		input[1..]
+			.iter()
+			.filter(|v| v.len() != 0)
+			.map(|s| {
+				s.split_whitespace()
+					.map(|s| s.parse::<i8>().unwrap())
+					.collect()
+			})
+			.collect(),
+	)
+}
+
 fn main() {
 	let input: Vec<String> = read("input/d04");
-
+	let (draw, cards) = split_input(&input);
+	let cards: &Vec<_> = &cards.chunks(5).collect();
+	
+	println!("{:?}", draw);
+	println!("{:?}", cards);
 }
