a4c340271dc2a28e38213b6b27e6a99b04f28128
nanderstabel <nanderstabel@gmail.com>
2021-12-19 09:42:29 +0100

    Add day 4

--- src/bin/d04.rs
+++ src/bin/d04.rs
@@ -1,6 +1,9 @@
+#![feature(drain_filter)]
 use aoc::*;
 
-fn split_input(input: &Vec<String>) -> (Vec<u8>, Vec<Vec<i8>>) {
+type Card<'a> = &'a mut [Vec<Option<u16>>];
+
+fn split_input(input: &Vec<String>) -> (Vec<u8>, Vec<Vec<Option<u16>>>) {
 	(
 		input[0]
 			.split(",")
@@ -11,16 +14,51 @@ fn split_input(input: &Vec<String>) -> (Vec<u8>, Vec<Vec<i8>>) {
 			.filter(|v| !v.is_empty())
 			.map(|s| {
 				s.split_whitespace()
-					.map(|s| s.parse::<i8>().unwrap())
+					.map(|s| Some(s.parse::<u16>().unwrap()))
 					.collect()
 			})
 			.collect(),
 	)
 }
 
+fn is_complete(array: &[Option<u16>]) -> bool {
+	array.iter().filter(|n| **n == None).count() == 5
+}
+
+fn complete(num: u8, card: Card) -> Option<u16> {
+	for y in 0..5 {
+		for x in 0..5 {
+			if card[y][x] == Some(num as u16) {
+				card[y][x] = None;
+				if is_complete(&[card[0][x], card[1][x], card[2][x], card[3][x], card[4][x]])
+					|| is_complete(&card[y])
+				{
+					return Some(card.iter().flatten().flat_map(|x| x).sum::<u16>() * num as u16);
+				}
+			}
+		}
+	}
+	None
+}
+
 fn main() {
 	let input: Vec<String> = read("input/d04");
 	let (draw, mut cards) = split_input(&input);
-	let mut cards: Vec<_> = cards.chunks_mut(5).collect();
+	let mut cards: Vec<Card> = cards.chunks_mut(5).collect();
 
+	let (mut first, mut last) = (0, 0);
+	for num in draw {
+		cards.drain_filter(|card| {
+			if let Some(result) = complete(num, card) {
+				if first == 0 {
+					first = result;
+				}
+				last = result;
+				true
+			} else {
+				false
+			}
+		});
+	}
+	output!(first, last);
 }
