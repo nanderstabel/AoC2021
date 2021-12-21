0d15e79f2a767de08c4eae63355b40e8f2c87884
nanderstabel <nanderstabel@gmail.com>
2021-12-21 15:54:59 +0100

    Add complete d11

--- src/bin/d11.rs
+++ src/bin/d11.rs
@@ -1,77 +1,73 @@
 use aoc::*;
 
-struct Arrangement(Vec<Vec<u8>>, u32);
+struct Arrangement {
+	map: Vec<Vec<u8>>,
+	steps: u32,
+	flashes: u32,
+}
 
 impl Arrangement {
 	pub fn from(input: Vec<String>) -> Self {
-		Arrangement(
-			input
+		Arrangement {
+			map: input
 				.iter()
 				.map(|s| s.chars().map(|c| c.to_digit(10).unwrap() as u8).collect())
 				.collect(),
-			0
-		)
+			steps: 0,
+			flashes: 0,
+		}
 	}
 
-	fn flash(&mut self, y: usize, x: usize) {
-		if self.0[y][x] == 10 {
+	fn flash(&mut self, y: isize, x: isize) {
+		if y < 0 || y == 10 || x < 0 || x == 10 || self.map[y as usize][x as usize] == 10 {
 			return;
 		}
-		self.0[y][x] += 1;
-		if self.0[y][x] == 10 {
-			self.1 += 1;
-			if y > 0 {
-				if x > 0 {
-					self.flash(y - 1, x - 1);
-				}
-				if x < 10 - 1 {
-					self.flash(y - 1, x + 1);
-				}
-				self.flash(y - 1, x);
-			}
-			if x > 0 {
-				self.flash(y, x - 1);
-			}
-			if x < 10 - 1 {
-				self.flash(y, x + 1);
-			}
-			if y < 10 - 1 {
-				if x > 0 {
-					self.flash(y + 1, x - 1);
-				}
-				if x < 10 - 1 {
-					self.flash(y + 1, x + 1);
-				}
-				self.flash(y + 1, x);
-			}
+		self.map[y as usize][x as usize] += 1;
+		if self.map[y as usize][x as usize] == 10 {
+			self.flashes += 1;
+			self.flash(y - 1, x - 1);
+			self.flash(y - 1, x);
+			self.flash(y - 1, x + 1);
+			self.flash(y, x - 1);
+			self.flash(y, x + 1);
+			self.flash(y + 1, x - 1);
+			self.flash(y + 1, x);
+			self.flash(y + 1, x + 1);
 		}
 	}
+}
 
-	pub fn take_steps(&mut self, steps: u8) {
-		for _ in 0..steps {
-			for y in 0..10 {
-				for x in 0..10 {
-					self.flash(y, x);
-				}
+impl Iterator for Arrangement {
+	type Item = u32;
+
+	fn next(&mut self) -> Option<Self::Item> {
+		for y in 0..10 {
+			for x in 0..10 {
+				self.flash(y, x);
 			}
-			for y in 0..10 {
-				for x in 0..10 {
-					if self.0[y][x] == 10 {
-						self.0[y][x] = 0;
-					}
+		}
+		for y in 0..10 {
+			for x in 0..10 {
+				if self.map[y][x] == 10 {
+					self.map[y][x] = 0;
 				}
 			}
 		}
-		for line in &self.0 {
-			println!("{:?}", line);
+		self.steps += 1;
+		if self.map.iter().flatten().filter(|&&x| x == 0).count() == 100 {
+			return None;
 		}
+		Some(self.flashes)
 	}
 }
 
 fn main() {
 	let mut arrangement = Arrangement::from(read("input/d11"));
-
-	arrangement.take_steps(100);
-
-	output!(arrangement.1, 42);
+	let mut step100 = 0;
+	while let Some(flashes) = arrangement.next() {
+		if arrangement.steps == 100 {
+			step100 = flashes;
+		}
+	}
+	output!(step100, arrangement.steps);
 }
